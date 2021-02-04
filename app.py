7f9415df4090f9b1fb6856b8096d4a0ea9044d4d
Daniel Miller <danielemiller@live.com>
2021-02-04 03:36:15 -0500

    initial deploy to heroku

--- app.py
+++ app.py
@@ -4,14 +4,14 @@ from send_mail import send_mail
 
 app = Flask(__name__)
 
-ENV = 'dev'
+ENV = 'prod'
 
 if ENV == 'dev':
     app.debug = True
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:xihuan1!@localhost/scl_feedback'
 else:    
     app.debug = false
-    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ruqbofatierhql:e8e39e149b93b7dcf7d89e104928664422a33e17c47519829c975e651218ace7@ec2-3-222-11-129.compute-1.amazonaws.com:5432/d1u2a70s4vsnpf'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
